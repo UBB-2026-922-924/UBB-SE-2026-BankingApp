a2eb2a9c14cfb9fc3b0bcf5df32378c67033a1dd
Allex3 <103828759+Allex3@users.noreply.github.com>
2026-06-01 06:36:36 +0300

    Forgot some semi-colons

--- BankingApp/src/BankingApp.Contracts/Features/Statistics/Services/IStatisticsService.cs
+++ BankingApp/src/BankingApp.Contracts/Features/Statistics/Services/IStatisticsService.cs
@@ -5,9 +5,9 @@ using ErrorOr;
 
 public interface IStatisticsService
 {
-    public Task<ErrorOr<SpendingByCategoryResponse>> GetSpendingByCategoryAsync(CancellationToken ct = default)
-    public Task<ErrorOr<IncomeVsExpensesResponse>> GetIncomeVsExpensesAsync(CancellationToken ct = default)
-    public Task<ErrorOr<BalanceTrendsResponse>> GetBalanceTrendsAsync(CancellationToken ct = default)
-    public Task<ErrorOr<TopRecipientsResponse>> GetTopRecipientsAsync(CancellationToken ct = default)
+    public Task<ErrorOr<SpendingByCategoryResponse>> GetSpendingByCategoryAsync(CancellationToken ct = default);
+    public Task<ErrorOr<IncomeVsExpensesResponse>> GetIncomeVsExpensesAsync(CancellationToken ct = default);
+    public Task<ErrorOr<BalanceTrendsResponse>> GetBalanceTrendsAsync(CancellationToken ct = default);
+    public Task<ErrorOr<TopRecipientsResponse>> GetTopRecipientsAsync(CancellationToken ct = default);
 
 }
