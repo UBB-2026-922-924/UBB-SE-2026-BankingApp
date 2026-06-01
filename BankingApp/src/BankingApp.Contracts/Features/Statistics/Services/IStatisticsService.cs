7a6c83733b056304c889285085dcb36c4b6a5cb9
Allex3 <103828759+Allex3@users.noreply.github.com>
2026-06-01 05:08:45 +0300

    Moved Statistics Client Services and used api client

---
+++ BankingApp/src/BankingApp.Contracts/Features/Statistics/Services/IStatisticsService.cs
@@ -0,0 +1,13 @@
+﻿namespace BankingApp.Contracts.Features.Statistics.Services;
+
+using BankingApp.Contracts.Features.Statistics.Dtos;
+using ErrorOr;
+
+public interface IStatisticsService
+{
+    public Task<ErrorOr<SpendingByCategoryResponse>> GetSpendingByCategoryAsync(CancellationToken ct = default)
+    public Task<ErrorOr<IncomeVsExpensesResponse>> GetIncomeVsExpensesAsync(CancellationToken ct = default)
+    public Task<ErrorOr<BalanceTrendsResponse>> GetBalanceTrendsAsync(CancellationToken ct = default)
+    public Task<ErrorOr<TopRecipientsResponse>> GetTopRecipientsAsync(CancellationToken ct = default)
+
+}
---
+++ BankingApp/src/BankingApp.Infrastructure.Http/Features/Statistics/Services/StatisticsService.cs
@@ -0,0 +1,49 @@
+﻿namespace BankingApp.Infrastructure.Http.Features.Statistics.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BankingApp.Application.Features.Authentication.Services;
+using BankingApp.Application.Shared.Http;
+using BankingApp.Contracts.Features.Statistics.Dtos;
+using BankingApp.Contracts.Features.Statistics.Services;
+using ErrorOr;
+
+public class StatisticsService(IApiClient apiClient) : IStatisticsService
+{
+
+    public Task<ErrorOr<SpendingByCategoryResponse>> GetSpendingByCategoryAsync(CancellationToken ct = default)
+    {
+        EnsureAuthenticatedSession();
+        return apiClient.GetAsync<SpendingByCategoryResponse>("/api/statistics/spending-by-category", ct);
+    }
+
+    public Task<ErrorOr<IncomeVsExpensesResponse>> GetIncomeVsExpensesAsync(CancellationToken ct = default)
+    {
+        EnsureAuthenticatedSession();
+        return apiClient.GetAsync<IncomeVsExpensesResponse>("/api/statistics/income-vs-expenses", ct);
+    }
+
+    public Task<ErrorOr<BalanceTrendsResponse>> GetBalanceTrendsAsync(CancellationToken ct = default)
+    {
+        EnsureAuthenticatedSession();
+        return apiClient.GetAsync<BalanceTrendsResponse>("/api/statistics/balance-trends", ct);
+    }
+
+    public Task<ErrorOr<TopRecipientsResponse>> GetTopRecipientsAsync(CancellationToken ct = default)
+    {
+        EnsureAuthenticatedSession();
+        return apiClient.GetAsync<TopRecipientsResponse>("/api/statistics/top-recipients", ct);
+    }
+
+    private void EnsureAuthenticatedSession()
+    {
+        //TODO no idea how to check if user is authenticated sorry
+        /*
+        if (!_authService.IsAuthenticated())
+        {
+            throw new UnauthorizedAccessException("An authenticated session is required.");
+        }
+        */
+    }
+}
