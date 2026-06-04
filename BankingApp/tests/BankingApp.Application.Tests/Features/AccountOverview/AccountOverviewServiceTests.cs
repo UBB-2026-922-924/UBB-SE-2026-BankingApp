5264c3e0658e8e4bf27be8dd6fe6533bd6188afb
Allex3 <103828759+Allex3@users.noreply.github.com>
2026-06-04 17:16:41 +0300

    Switched NUnit to XUnit to adhere to project config

--- BankingApp/tests/BankingApp.Application.Tests/Features/AccountOverview/AccountOverviewServiceTests.cs
+++ BankingApp/tests/BankingApp.Application.Tests/Features/AccountOverview/AccountOverviewServiceTests.cs
@@ -15,76 +15,67 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using NodaMoney;
 using NSubstitute;
-using NUnit.Framework;
-using NUnit.Framework.Internal;
 using CardType = Domain.Enums.CardType;
 using Currency = NodaMoney.Currency;
 using Transaction = Domain.Aggregates.AccountAggregate.Entities.Transaction;
-
-[TestFixture]
-public class AccountOverviewServiceTests
+public sealed class AccountOverviewServiceTests
 {
-    private IUserRepository mockUserRepository = Substitute.For<IUserRepository>();
-    private IAccountRepository mockAccountRepository = Substitute.For<IAccountRepository>();
-    private ITransactionRepository mockTransactionRepository = Substitute.For<ITransactionRepository>();
+    private readonly Mock<IUserRepository> mockUserRepository;
+    private readonly Mock<IAccountRepository> mockAccountRepository;
+    private readonly Mock<ITransactionRepository> mockTransactionRepository;
 
-    private AccountOverviewService accountOverviewService = null!;
-
-    [SetUp]
-    public void SetUp()
+    private readonly AccountOverviewService accountOverviewService;
+    public AccountOverviewServiceTests()
     {
-        mockUserRepository = Substitute.For<IUserRepository>();
-        mockAccountRepository = Substitute.For<IAccountRepository>();
-        mockTransactionRepository = Substitute.For<ITransactionRepository>();
+        mockUserRepository = MockFactory.CreateUserRepositoryMock();
+        mockAccountRepository = MockFactory.CreateAccountRepositoryMock();
+        mockTransactionRepository = MockFactory.CreateTransactionRepositoryMock();
 
         accountOverviewService = new AccountOverviewService(
-            mockUserRepository,
-            mockAccountRepository,
-            mockTransactionRepository,
+            mockUserRepository.Object,
+            mockAccountRepository.Object,
+            mockTransactionRepository.Object,
             NullLogger< AccountOverviewService>.Instance);
     }
 
-    [Test]
-    public void GetDashboard_WhenUserIsNull_ReturnsNotFoundError()
+    [Fact]
+    public async Task GetDashboard_WhenUserIsNull_ReturnsNotFoundError()
     {
-        mockUserRepository.GetByIdAsync(1).Returns((User)null!);
+        mockUserRepository.Setup(r => r.GetByIdAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((User)null!);
 
-        ErrorOr<AccountOverviewDto> getDashboardResponse = accountOverviewService.GetDashboardAsync(1).Result;
+        ErrorOr<AccountOverviewDto> getDashboardResponse = await accountOverviewService.GetDashboardAsync(1, TestContext.Current.CancellationToken);
 
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(getDashboardResponse.IsError, Is.True);
-            Assert.That(getDashboardResponse.FirstError, Is.EqualTo(UserErrors.NotFound));
-        }
+        getDashboardResponse.IsError.Should().BeTrue();
+        getDashboardResponse.FirstError.Should().Be(UserErrors.NotFound);
     }
 
-    [Test]
-    public void GetDashboard_WhenUserExists_ReturnsTransactions()
+    [Fact]
+    public async Task GetDashboard_WhenUserExists_ReturnsTransactions()
     {
 
         var user = User.Register(Email.Create("example@example.com").Value, "full", DateTime.Today);
-        mockUserRepository.GetByIdAsync(1).Returns(user);
+        mockUserRepository.Setup(r => r.GetByIdAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(user);
 
-        var account = Account.Open(0, Iban.Create("RO20GJSDGNSDJAFFBDGJSDGBGFJDFFG").Value, new Currency(),
+        var account = Account.Open(0, Iban.Create("RO12BANK1234567890123456").Value, new Currency(),
             AccountType.Checking, "name", new DateTime());
-     
-        var accounts = new ReadOnlyCollection<Account>(new List<Account>([account]));
 
         account.IssueCard("aaa", "aaaa", new DateTime(2028, 1, 1), "222", CardType.Debit, "brand", new DateTime());
 
-        mockAccountRepository.ListByUserIdAsync(0).Returns(accounts);
+        var accounts = new ReadOnlyCollection<Account>(new List<Account>([account]));
+
+        mockAccountRepository.Setup(r => r.ListByUserIdAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(accounts);
 
         var transaction = Domain.Aggregates.AccountAggregate.Entities.Transaction.Create(0, "dhdfsd", "dggf",
             TransactionDirection.In, new Money(10), new Money(20), TransactionStatus.Completed, new DateTime());
-        mockTransactionRepository.ListByAccountIdAsync(0).Returns(new ReadOnlyCollection<Domain.Aggregates.AccountAggregate.Entities.Transaction>(new List<Transaction>([transaction])));
+        mockTransactionRepository.Setup(r => r.ListByAccountIdAsync(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ReadOnlyCollection<Domain.Aggregates.AccountAggregate.Entities.Transaction>(new List<Transaction>([transaction])));
 
-        ErrorOr<AccountOverviewDto> getDashboardResponse = accountOverviewService.GetDashboardAsync(1).Result;
+        ErrorOr<AccountOverviewDto> getDashboardResponse = await accountOverviewService.GetDashboardAsync(1, TestContext.Current.CancellationToken);
 
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(getDashboardResponse.IsSuccess, Is.True);
-            Assert.That(getDashboardResponse.Value.Cards.Count(f => true), Is.GreaterThan(0));
-            Assert.That(getDashboardResponse.Value.RecentTransactions.Count(f => true), Is.GreaterThan(0));
-        }
+        getDashboardResponse.IsSuccess.Should().BeTrue();
+        getDashboardResponse.Value.Cards.Should().HaveCountGreaterThan(0);
+        getDashboardResponse.Value.RecentTransactions.Should().HaveCountGreaterThan(0);
     }
 }
