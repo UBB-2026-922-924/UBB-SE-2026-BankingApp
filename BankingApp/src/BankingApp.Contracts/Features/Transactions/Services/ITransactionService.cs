5e2aa933c3c652997bb97083adb21f1b7845a6e6
Kovács Levente <le.kovacs789@gmail.com>
2026-06-07 05:26:25 +0300

    created ITransactionService in Contracts project

---
+++ BankingApp/src/BankingApp.Contracts/Features/Transactions/Services/ITransactionService.cs
@@ -0,0 +1,13 @@
+﻿namespace BankingApp.Contracts.Features.Transactions.Services;
+
+using Dtos;
+using ErrorOr;
+
+public interface ITransactionService
+{
+    public Task<TransactionFilterMetadataResponse?> GetFilterMetadataAsync(CancellationToken ct = default);
+    public Task<TransactionHistoryResponse?> GetHistoryAsync(TransactionHistoryRequest request, CancellationToken ct = default);
+    public Task<TransactionDetailsResponse?> GetTransactionByIdAsync(int transactionId, CancellationToken ct = default);
+    public Task<ExportedFileResult?> ExportTransactionsAsync(TransactionExportRequest request, CancellationToken ct = default);
+    public Task<ExportedFileResult?> ExportReceiptAsync(int transactionId, CancellationToken ct = default);
+}
