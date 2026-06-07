ec34465e3d023e810f818134c41006916043e0fe
Kovács Levente <le.kovacs789@gmail.com>
2026-06-07 04:16:37 +0300

    brought missing DTOs over from past project

---
+++ BankingApp/src/BankingApp.Contracts/Features/Transactions/Dtos/ExportedFileResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BankingApp.Contracts.Features.Transactions.Dtos;
+
+public sealed class ExportedFileResult
+{
+    public string FileName { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+}
